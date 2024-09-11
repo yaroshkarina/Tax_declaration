9e547403bb2ee90a5109a0f49082b7c5ce5fa4d9
Karina Yarosh <yarosh.karina@gmail.com>
2024-09-11 22:40:23 +0300

    Initial Commit

---
+++ Tax_declaration/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tax_declaration
+//
+//  Created by Каріна Ярош on 11.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
