0cd285ab08d7478ccc37c3e2b7bed50d978ba54d
Karina Yarosh <yarosh.karina@gmail.com>
2024-09-11 22:44:35 +0300

    git add main.cpp git commit -m "Added main program logic for testing TaxDeclaration system"

--- Tax_declaration/main.cpp
+++ Tax_declaration/main.cpp
@@ -1,14 +1,40 @@
-//
-//  main.cpp
-//  Tax_declaration
-//
-//  Created by Каріна Ярош on 11.09.2024.
-//
-
 #include <iostream>
+#include <string>
+#include "TaxDeclarationSystem.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    TaxDeclarationSystem system;
+
+    int numDeclarations;
+    cout << "Enter the number of tax declarations you want to submit: ";
+    cin >> numDeclarations;
+
+    for (int i = 0; i < numDeclarations; ++i) {
+        string name, id;
+        double income, expenses;
+
+        cout << "Enter taxpayer name: ";
+        cin.ignore();
+        getline(cin, name);
+
+        cout << "Enter taxpayer ID: ";
+        getline(cin, id);
+
+        cout << "Enter income: ";
+        cin >> income;
+
+        cout << "Enter expenses: ";
+        cin >> expenses;
+
+        TaxDeclaration declaration(name, id, income, expenses);
+        system.submitDeclaration(declaration);
+    }
+
+    system.viewDeclarations();
+
+    system.generateReport();
+
     return 0;
 }
